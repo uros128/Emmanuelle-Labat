cedf3525de1e86c6a99a920d568049447e897d4c
uros128 <32303084+uros128@users.noreply.github.com>
2019-10-05 17:58:48 +0100

    Add files via upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "Thank you for your email'"; //letter subject
+	$to = 'emmanuelle.labat.orthophonie@gmail.com'; // Enter Your E-mail
+	$from = 'admin@you-site-name.com'; // Admin e-mail
+	$fromName = 'Your Company Name'; // Your company name
+	$charset = 'UTF-8';
+?>
